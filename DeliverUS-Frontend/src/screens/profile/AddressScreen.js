ab4e0c73afa494bbe891944e4a78a821a3ff38a6
Gustavo Delgado <158163450+GustavoD1@users.noreply.github.com>
2025-10-25 18:21:41 +0200

    v2

--- DeliverUS-Frontend/src/screens/profile/AddressScreen.js
+++ DeliverUS-Frontend/src/screens/profile/AddressScreen.js
@@ -1,199 +1,193 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { StyleSheet, View, FlatList, Pressable } from 'react-native'
-import { brandPrimary, brandPrimaryTap, brandSecondary, flashStyle, flashTextStyle } from '../../styles/GlobalStyles'
+import { brandPrimary, brandPrimaryTap, brandSecondary } from '../../styles/GlobalStyles'
 import TextRegular from '../../components/TextRegular'
 import TextSemiBold from '../../components/TextSemibold'
-import TextError from '../../components/TextError'
 import { getAddresses, setDefault, deleteAddress } from '../../api/AddressEndpoints'
 import { showMessage } from 'react-native-flash-message'
 import { Ionicons } from '@expo/vector-icons'
 import DeleteModal from '../../components/DeleteModal'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import { AuthorizationContext } from '../../context/AuthorizationContext'
 
 export default function AddressScreen ({ navigation, route }) {
   const [addresses, setAddresses] = useState([])
-  const [error, setError] = useState(null)
   const [addressToBeDeleted, setAddressToBeDeleted] = useState(null)
+  const { loggedInUser } = useContext(AuthorizationContext)
 
   useEffect(() => {
-    fetchAddresses()
-  }, [route])
+    if (loggedInUser) {
+      fetchAddresses()
+    } else {
+      setAddresses(null)
+    }
+  }, [loggedInUser, route])
 
   async function fetchAddresses () {
     try {
-      const fetchedAddresses = await getAddresses()
-
-      setAddresses(fetchedAddresses)
+      const data = await getAddresses()
+      setAddresses(data)
     } catch (error) {
       showMessage({
-        message: `There was an error while retrieving addresses. ${error} `,
-        type: 'error',
-        style: flashStyle,
-        titleStyle: flashTextStyle
+        message: 'Error fetching addresses',
+        description: error.toString(),
+        type: 'danger',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
       })
     }
   }
 
-  const renderHeader = () => {
-    return (
-        <View>
-          <TextSemiBold textStyle={styles.textTitle}>{'Mis direcciones'}</TextSemiBold>
-        </View>
-    )
-  }
-
-  const handleSetDefault = async (values) => {
+  const removeAddress = async (item) => {
     try {
-      await setDefault(values.id)
-      fetchAddresses()
-      showMessage({
-        message: 'Dirección predeterminada actualizada correctamente.',
-        type: 'success',
-        style: flashStyle,
-        titleStyle: flashTextStyle
-      })
+      await deleteAddress(item.id)
+      await fetchAddresses()
+      setAddressToBeDeleted(null)
+      showMessage(
+        {
+          message: `Dirección ${item.alias} eliminada`,
+          type: 'success',
+          style: GlobalStyles.flashStyle,
+          titleStyle: GlobalStyles.flashTextStyle
+        })
     } catch (error) {
+      setAddressToBeDeleted(null)
       showMessage({
-        message: `Error al establecer la dirección como predeterminada: ${error}`,
-        type: 'error',
-        style: flashStyle,
-        titleStyle: flashTextStyle
+        message: `Error al eliminar ${item.alias}: ${error}`,
+        type: 'danger',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
       })
     }
   }
 
-  const renderAddresses = ({ item }) => {
-    return (
-        <View style ={{ flexDirection: 'row', padding: 20, justifyContent: 'space-between', marginLeft: 20, marginRight: 20 }}>
-      <View >
-        <TextSemiBold>{item.alias}</TextSemiBold>
-      </View>
-      <View >
-        <TextRegular>{item.street}, {item.city}, {item.province}, {item.zipCode}</TextRegular>
-      </View>
-      <View style={{ flexDirection: 'row' }}>
+  const setAddressAsDefault = async (item) => {
+    try {
+      await setDefault(item.id)
+      await fetchAddresses()
+      showMessage({
+        message: `Dirección ${item.alias} establecida como predeterminada`,
+        type: 'success',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    } catch (error) {
+      showMessage({
+        message: `Error al establecer ${item.alias} predeterminada: ${error}`,
+        type: 'danger',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
+    }
+  }
+
+  const renderAddress = ({ item }) => (
+    <View style={styles.addressContainer}>
+      <TextSemiBold>{item.alias}</TextSemiBold>
+      <TextRegular>{item.street}, {item.city}, {item.province}, {item.zipCode}</TextRegular>
+      <View style={styles.actions}>
         <Pressable
-            onPress={() => handleSetDefault(item)}
-            style={({ pressed }) => [
-              {
-                backgroundColor: 'transparent'
-              },
-              styles.actionButton
-            ]}>
-              <Ionicons name= {item.isDefault ? 'star' : 'star-outline'} size={24} color={'#FFC107'} />
-          </Pressable>
-          <Pressable
-            onPress={(() => setAddressToBeDeleted(item))}
-            style={({ pressed }) => [
-              {
-                backgroundColor: 'transparent'
-              },
-              styles.actionButton
-            ]}>
-              <Ionicons name= 'trash-outline' size={24} color={'red'} />
-          </Pressable>
+          onPress={() => setAddressAsDefault(item)}
+          style={({ pressed }) => [{
+            padding: 5,
+            borderRadius: 4,
+            backgroundColor: pressed ? brandPrimaryTap : 'transparent'
+          }]}
+        >
+          <Ionicons name={item.isDefault ? 'star' : 'star-outline'} size={24} color={brandPrimary} />
+        </Pressable>
+        <Pressable
+          onPress={() => setAddressToBeDeleted(item)}
+
+          style={({ pressed }) => [{
+            padding: 5,
+            borderRadius: 4,
+            backgroundColor: pressed ? 'rgba(255,0,0,0.2)' : 'transparent'
+          }]}
+        >
+          <Ionicons name="trash" size={24} color="red" />
+        </Pressable>
       </View>
-      </View>
+    </View>
+  )
 
-    )
-  }
-
-  const renderSeparator = () => {
-    return <View style={styles.separator} />
-  }
-
-  const removeAddress = async (value) => {
-    try {
-      await deleteAddress(value.id)
-      setAddressToBeDeleted(null)
-      fetchAddresses()
-      showMessage({
-        message: `Address ${value.alias} succesfully removed`,
-        type: 'success',
-        style: flashStyle,
-        titleStyle: flashTextStyle
-      })
-    } catch (error) {
-      console.log(error)
-      setAddressToBeDeleted(null)
-      showMessage({
-        message: `Address ${value.alias} could not be removed.`,
-        type: 'error',
-        style: flashStyle,
-        titleStyle: flashTextStyle
-      })
-    }
-  }
+  const renderEmptyList = () => (
+    <TextRegular textStyle={styles.emptyList}>No tienes direcciones guardadas.</TextRegular>
+  )
 
   return (
-    <>
     <View style={styles.container}>
-          <FlatList
-              ItemSeparatorComponent={renderSeparator}
-              ListHeaderComponent={renderHeader}
-              style={styles.container}
-              data={addresses}
-              renderItem={renderAddresses}
-              keyExtractor={item => item.id.toString()}
-            />
-            <Pressable
-            onPress={() => navigation.navigate('AddressDetailScreen')}
-            style={({ pressed }) => [
-              {
-                backgroundColor: pressed
-                  ? brandPrimaryTap
-                  : brandPrimary
-              },
-              styles.button
-            ]}>
-            {<TextRegular textStyle={styles.textButtom}>
-              Añadir nueva dirección
-            </TextRegular> }
-          </Pressable>
-          <DeleteModal
-            isVisible={addressToBeDeleted !== null}
-            onCancel={() => setAddressToBeDeleted(null)}
-            onConfirm={() => removeAddress(addressToBeDeleted)}>
-              <TextRegular>¿Seguro que quieres eliminar esta dirección?</TextRegular>
-            </DeleteModal>
-        </View>
-    </>
+      <TextSemiBold textStyle={styles.title}>Mis direcciones</TextSemiBold>
+
+      <FlatList
+        data={addresses}
+        renderItem={renderAddress}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={addresses.length === 0 && styles.emptyContainer}
+        ListEmptyComponent={renderEmptyList}
+      />
+      <Pressable
+        onPress={() => navigation.navigate('AddressDetailScreen')}
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? brandPrimaryTap : brandPrimary },
+          styles.button
+        ]}
+      >
+        <TextRegular textStyle={styles.textButtom}>Añadir nueva dirección</TextRegular>
+      </Pressable>
+      <DeleteModal
+       isVisible={addressToBeDeleted !== null}
+       onCancel={() => setAddressToBeDeleted(null)}
+       onConfirm={() => {
+         removeAddress(addressToBeDeleted)
+       }}
+     >
+       <TextRegular>¿Seguro que quieres eliminar esta dirección?</TextRegular>
+     </DeleteModal>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  textTitle: {
-    fontSize: 20,
-    color: 'black',
-    padding: 15
-  },
   container: {
-    flex: 1
+    flex: 1,
+    padding: 20
   },
-  separator: {
-    height: 1,
-    backgroundColor: '#e0e0e0',
-    marginHorizontal: 15
+  title: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  addressContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  actions: {
+    flexDirection: 'row',
+    gap: 10
   },
   button: {
-    textAlign: 'center',
     borderRadius: 8,
-    marginBottom: 5,
-    height: 40,
-    width: '95%',
     padding: 10,
-    alignSelf: 'center',
-    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: 15
   },
   textButtom: {
     fontSize: 16,
     color: brandSecondary,
     textAlign: 'center'
   },
-  actionButton: {
-    padding: 8,
-    marginLeft: 10
+  emptyList: {
+    textAlign: 'center',
+    padding: 50
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
-
 })
